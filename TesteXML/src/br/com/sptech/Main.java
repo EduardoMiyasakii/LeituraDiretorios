eae0d06707eb5b1bbc576ab61641dab824ef76d0
EduardoMiyasakii <eduardo.miyasaki@sptech.school>
2025-08-27 22:58:29 -0300

    Finalizei

--- TesteXML/src/br/com/sptech/Main.java
+++ TesteXML/src/br/com/sptech/Main.java
@@ -1,7 +1,6 @@
 package br.com.sptech;
 
 import java.io.File;
-
 import javax.swing.JFileChooser;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -12,97 +11,87 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 public class Main {
 
-	public static void main(String[] args) throws ParserConfigurationException, TransformerException {
+    public static void main(String[] args) throws ParserConfigurationException, TransformerException {
 
-		JFileChooser chooser = new JFileChooser();
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 
-		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int retorno = chooser.showOpenDialog(null);
 
-		int retorno = chooser.showOpenDialog(null);
+        if (retorno == JFileChooser.APPROVE_OPTION) {
+            File pastaSelecionada = chooser.getSelectedFile();
+            System.out.println("Pasta selecionada: " + pastaSelecionada.getAbsolutePath());
 
-		if (retorno == JFileChooser.APPROVE_OPTION) {
-			File pastaSelecionada = chooser.getSelectedFile();
-			System.out.println("Pasta selecionada: " + pastaSelecionada.getAbsolutePath());
-			try {
-				listarSubdiretorios(pastaSelecionada);
-			} catch (ParserConfigurationException | TransformerException e) {
-				e.printStackTrace();
-			}
-		} else {
-			System.out.println("Nenhuma pasta foi selecionada.");
-		}
-	}
+            gerarXML(pastaSelecionada);
+        } else {
+            System.out.println("Nenhuma pasta foi selecionada.");
+        }
+    }
 
-	private static void listarSubdiretorios(File pasta)
-			throws ParserConfigurationException, TransformerException {
-		
-		File[] arquivos = pasta.listFiles();
+    private static void gerarXML(File pasta) throws ParserConfigurationException, TransformerException {
 
-		if (arquivos != null) {
+    	// criando documento
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        Document doc = docBuilder.newDocument();
 
-			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        // elemento raiz
+        Element rootElement = doc.createElement("eclipse-userlibraries");
+        rootElement.setAttribute("version", "2");
+        doc.appendChild(rootElement);
 
-			Document doc = (Document) docBuilder.newDocument();
+        // listando diretórios e arquivos
+        File[] arquivos = pasta.listFiles();
+        if (arquivos != null) {
+            for (File arquivo : arquivos) {
+                if (arquivo.isDirectory()) {
+                    System.out.println("Diretório: " + arquivo.getName());
 
-			Element rootElement = doc.createElement("?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?");
-			doc.appendChild(rootElement);
+                    // <library name="ibpj_banc_contratacao" systemlibrary="false">
+                    Element library = doc.createElement("library");
+                    library.setAttribute("name", arquivo.getName());
+                    library.setAttribute("systemlibrary", "false");
+                    rootElement.appendChild(library);
 
-			Element eclipseVersion = doc.createElement("<eclipse-userlibraries version=\"2\">");
-			rootElement.appendChild(eclipseVersion);
+                    // adicionando arquivos desse diretório
+                    listarArquivos(arquivo, doc, library);
+                }
+            }
+        }
 
-			System.out.println("Subdiretórios encontrados:");
-			for (File arquivo : arquivos) {
+        // salvando XML
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
 
-				if (arquivo.isDirectory()) {
-					Element libraryName = doc
-							.createElement("<library name=\"${arquivo.getName()} \" systemlibrary=\"false\">");
-					rootElement.appendChild(libraryName);
-					System.out.println(" - " + arquivo.getName());
-					listarArquivos(arquivo);
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(new File("all.userlibraries.xml"));
+        transformer.transform(source, result);
 
-					TransformerFactory transformerFactory = TransformerFactory.newInstance();
-					Transformer transformer = transformerFactory.newTransformer();
+        System.out.println("Arquivo XML gerado!!!!");
+    }
 
-					transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+    private static void listarArquivos(File subDiretorio, Document doc, Element library) {
+    	
+        File[] arquivos = subDiretorio.listFiles();
+        
+        if (arquivos != null) {
+            for (File arquivo : arquivos) {
+                if (arquivo.isFile() && arquivo.getName().endsWith(".jar")) {
+                    System.out.println(" JAR encontrado: " + arquivo.getAbsolutePath());
 
-					DOMSource source = new DOMSource(doc);
-					StreamResult result = new StreamResult(new File("all.userlibraries.xml"));
-
-					transformer.transform(source, result);
-					System.out.println("Arquivo XML gerado com sucesso!");
-				}
-			}
-		} else {
-			System.out.println("Não foi possível listar os arquivos da pasta.");
-		}
-	}
-
-	private static void listarArquivos(File subDiretorio) throws ParserConfigurationException {
-		
-		File[] arquivos = subDiretorio.listFiles();
-		
-		if(arquivos != null) {
-			
-			System.out.println("Jars encontrados:");
-			for(File arquivo: arquivos) {
-				
-				if(arquivo.isFile()) {
-					String jarNome = arquivo.getName()
-					Element jarName = doc.createElement("<archive path='${jarNome}'/>"); 
-					rootElement.appendChild(jarName);
-				}
-				else if(arquivo.isDirectory()){
-					listarArquivos(arquivo.getAbsolutePath());
-				}
-			}
-		}
-	}
+                    Element archive = doc.createElement("archive");
+                    archive.setAttribute("path", arquivo.getAbsolutePath());
+                    library.appendChild(archive);
+                } else if (arquivo.isDirectory()) {
+                    listarArquivos(arquivo, doc, library);
+                }
+            }
+        }
+    }
 }
-
